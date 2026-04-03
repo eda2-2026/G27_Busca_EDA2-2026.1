bb55ed2b53ee44ebafd7d02d36cdbc5c574e2342
juliagabriellafs <juliagabriellaf0407@gmail.com>
2026-04-03 00:41:20 -0300

    feat - implementa estrutura para a tabela hash

--- backend/include/busca_hash.h
+++ backend/include/busca_hash.h
@@ -11,4 +11,10 @@ typedef struct ListaEnc_hash {
     struct NoHash *proximo;     // para o caso de colisão
 } ListaEnc_hash;
 
+//monta a tabela hash de tamanho n
+typedef struct {
+    ListaEnc_hash **inicio;     // guarda início das listas
+    size_t tamanho_total;        // tamanho total do hash
+} TabelaHash;
+
 #endif
